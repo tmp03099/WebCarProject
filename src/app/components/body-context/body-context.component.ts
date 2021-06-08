ffad69a8ee9fb7a3cd274dc45e7cde7559895f3c
tmp03099 <tmp03099@gmail.com>
2021-06-08 19:35:57 -0400

    Design icon and body updated

--- src/app/components/body-context/body-context.component.ts
+++ src/app/components/body-context/body-context.component.ts
@@ -14,7 +14,7 @@ export class BodyContextComponent implements OnInit {
     "assets/img/banners/TOP-BANNER-compress.jpg"
   ]
 
-  modelCars: ModelCarInterface[] = [
+  model5SeatsCars: ModelCarInterface[] = [
     {
       imageSrc: 'assets/img/cars/corolla altis.png', 
       name: 'TOYOTA VIOS', 
@@ -39,13 +39,75 @@ export class BodyContextComponent implements OnInit {
       imageSrc: 'assets/img/cars/yaris.png', 
       name: 'TOYOTA YARIS', 
       price: '668.000.000'
+    }
+  ]
+
+  modelSUVCars: ModelCarInterface[] = [
+    {
+      imageSrc: 'assets/img/cars/Rush.png', 
+      name: 'TOYOTA RUSH', 
+      price: '478.000.000'
     },
     {
-      imageSrc: 'assets/img/cars/WHILE-070-2.png', 
-      name: 'XE US', 
-      price: ''
+      imageSrc: 'assets/img/cars/Fortuner.png', 
+      name: 'TOYOTA FORTUNER', 
+      price: '1.029.000.000'
     },
+    {
+      imageSrc: 'assets/img/cars/Corolla Cross.jpg', 
+      name: 'TOYOTA COROLLA CROSS', 
+      price: '733.000.000'
+    },
+    {
+      imageSrc: 'assets/img/cars/Land Cruiser Prado VX.png', 
+      name: 'TOYOTA LAND CRUISER PRADO VX', 
+      price: '352.000.000'
+    },
+    {
+      imageSrc: 'assets/img/cars/Land Cruiser.png', 
+      name: 'TOYOTA LAND CRUISER', 
+      price: '352.000.000'
+    }
   ]
+
+  model16SeatsCars: ModelCarInterface[] = [
+    {
+      imageSrc: 'assets/img/cars/innova.png', 
+      name: 'TOYOTA INNOVA', 
+      price: '478.000.000'
+    },
+    {
+      imageSrc: 'assets/img/cars/avanza.png', 
+      name: 'TOYOTA AVANZA', 
+      price: '1.029.000.000'
+    },
+    {
+      imageSrc: 'assets/img/cars/alphard luxury.png', 
+      name: 'TOYOTA ALPHARD LUXURY', 
+      price: '733.000.000'
+    }
+  ]
+
+  modelTrucksCars: ModelCarInterface[] = [
+    {
+      imageSrc: 'assets/img/cars/hilux.png', 
+      name: 'TOYOTA HILUX', 
+      price: '478.000.000'
+    },
+    {
+      imageSrc: 'assets/img/cars/hiace.jpg', 
+      name: 'TOYOTA HIACE', 
+      price: '1.029.000.000'
+    },
+    {
+      imageSrc: 'assets/img/cars/Granvia Premium package.png', 
+      name: 'GRANVIA PREMIUM PACKAGE', 
+      price: '733.000.000'
+    }
+  ]
+
+
+
 
   constructor()  { }
 
