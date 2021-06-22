fdff49ad7fd92a018c0365345bcc72239a9a0625
tmp03099 <tmp03099@gmail.com>
2021-06-21 23:20:59 -0400

    updated prices & drive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentModule } from './components';
-import { BangGiaModule, HomeModule, IntroductionModule } from './pages';
+import { BangGiaModule, HomeModule, IntroductionModule, LaiThuComponent } from './pages';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TraGopComponent } from './pages/tra-gop/tra-gop.component';
 import { KhuyenMaiComponent } from './pages/khuyen-mai/khuyen-mai.component';
@@ -12,6 +12,8 @@ import { XeQuaSuDungComponent } from './pages/xe-qua-su-dung/xe-qua-su-dung.comp
 import { TinTucComponent } from './pages/tin-tuc/tin-tuc.component';
 import { LienHeComponent } from './pages/lien-he/lien-he.component';
 import { ToyotaModule } from './pages/toyota/toyota.module';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { ToyotaModule } from './pages/toyota/toyota.module';
     XeQuaSuDungComponent,
     TinTucComponent,
     LienHeComponent,
+    LaiThuComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +33,9 @@ import { ToyotaModule } from './pages/toyota/toyota.module';
     IntroductionModule,
     BangGiaModule,
     FontAwesomeModule,
-    ToyotaModule
+    ToyotaModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
