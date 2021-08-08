dfed7a0ba69d3e7c497c05785b74cc579409223c
Lam Ly <ldl5007@gmail.com>
2021-08-07 21:40:35 -0400

    update

--- src/app/pages/toyota/common-components/noi-that/noi-that.component.ts
+++ src/app/pages/toyota/common-components/noi-that/noi-that.component.ts
@@ -7,6 +7,24 @@ import { CarNgoaiThatInterface } from "src/app/services";
   styleUrls: ['noi-that.component.scss']
 })
 export class NoiThatComponent {
+  responsiveOptions = [
+    {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+    }
+  ];
+
   @Input()
   public noiThat: CarNgoaiThatInterface;
 
